5996807b806bb6426cb5d25ac14ca66a27c89389
CarlosMoreno24 <morenopachecocarlosmanuel@yahoo.com>
2025-09-18 19:04:07 -0600

    Cambios en contactos e index

--- Config/conexion.php
+++ Config/conexion.php
@@ -1,17 +1,22 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pwd = "";
-$DB = "agora";
+$host = "72.60.123.239";   
+$port = 3306;              
+$user = "crm_user";        
+$pwd  = "90a49c9fa750d43e34db";     
+$DB   = "agora";           
 
 
-$connection = new mysqli($host, $user, $pwd, $DB);
-$connection->set_charset("utf8");
+$connection = new mysqli($host, $user, $pwd, $DB, $port);
 
-    if($connection->connect_errno){
-         die("Conexión fallida: " . mysqli_connect_error());
 
-    }
+if ($connection->connect_errno) {
+    error_log("Error de conexión MySQL ({$connection->connect_errno}): {$connection->connect_error}");
+    die("Error de conexión con la base de datos. Contacta al administrador.");
+}
 
-    $db = mysqli_select_db($connection, $DB);
+
+if (!$connection->set_charset("utf8mb4")) {
+    error_log("Error configurando charset: " . $connection->error);
+}
+
 ?>
